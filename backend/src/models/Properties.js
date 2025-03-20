ccbf9a4ec05b61371db9488800461b0dbbdf8af1
xxDianaPadilla <dianagabypadilla006@gmail.com>
2025-03-20 11:17:28 -0600

    Avances del crud de la colección de properties

---
+++ backend/src/models/Properties.js
@@ -0,0 +1,65 @@
+import {Schema, model} from "mongoose";
+
+const propertiesSchema = new Schema({
+    images: [
+        {
+            image: {
+                type: String,
+                required: true,
+            },
+        },
+    ],
+    description: {
+        type: String, 
+        require: true
+    },
+    location: {
+        type: String,
+        require: true
+    },
+    price: {
+        type: String,
+        require: true
+    },
+    floors: {
+        type: Number,
+        require: true
+    },
+    flooringType: {
+        type: String,
+        require: true
+    },
+    lotSize: {
+        type: String,
+        require: true
+    },
+    height: {
+        type: String,
+        require: true
+    },
+    constructionYear: {
+        type: String,
+        require: true
+    },
+    rooms: {
+        type: Number,
+        require: true
+    },
+    bathrooms: {
+        type: Number, 
+        require: true
+    },
+    parkingLot: {
+        type: Boolean,
+        require: true
+    },
+    patio: {
+        type: Boolean,
+        require: true
+    },
+    sellerId: {
+        type: Schema.Types.ObjectId,
+        ref: "",
+        require: true
+    }
+})
