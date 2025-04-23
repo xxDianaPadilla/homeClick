c06dd156f36b20394879b679988d23a2a35c47e7
xxDianaPadilla <dianagabypadilla006@gmail.com>
2025-04-23 15:36:44 -0600

    Creación de componente para nav

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/EstiloNav.css";
+import homeClickLogo from '../assets/naranja.png';
+import searchIcon from '../assets/image1.png';
+import saveIcon from '../assets/image23.png';
+import cartIcon from '../assets/image24.png';
+import profileIcon from '../assets/image3.png';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className="navbar-logo">
+                    <a href="/">
+                        <img src={homeClickLogo} alt="HomeClick Logo" />
+                    </a>
+                </div>
+
+                <div className="search-container">
+                    <input type="text" placeholder="Buscar..."/>
+                    <button className="search-button">
+                        <img src={searchIcon} alt="" />
+                    </button>
+                </div>
+
+                <div className="navbar-icons">
+                    <button className="icon-button bookmark-button">
+                        <img src={saveIcon} alt="" />
+                    </button>
+                    <button className="icon-button cart-button">
+                        <img src={cartIcon} alt="" />
+                    </button>
+                    <button className="icon-button profile-button">
+                        <img src={profileIcon} alt="" />
+                    </button>
+                </div>
+            </div>
+
+            <hr className="espacio"/>
+
+            <div className="navbar-menu">
+                <ul className="navbar-options">
+                    <li><a href="/casa-de-campo">Casa de campo</a></li>
+                    <li><a href="/apartamento">Apartamentos</a></li>
+                    <li><a href="/casa-de-playa">Casa de playa</a></li>
+                    <li><a href="/mansion">Mansión</a></li>
+                    <li><a href="/cabana">Cabaña</a></li>
+                    <li><a href="/loft">Loft</a></li>
+                    <li><a href="/casa-prefabricada">Casa prefabricada</a></li>
+                    <li><a href="/villa">Villa</a></li>
+                    <li><a href="/duplex">Dúplex</a></li>
+                    <li><a href="/townhouse">Townhouse</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from '../components/Navbar';
+import '../styles/EstiloLandingPage.css';
+
+const LandingPage = () => {
+    return(
+        <div className="landing-page">
+            <Navbar/>
+        </div>
+    );
+};
+
+export default LandingPage;
