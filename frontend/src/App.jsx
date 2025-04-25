feb3d122f7edc252ecb31517e34f11d59af7c916
Rodrigo Monterrosa <rjmj.007.009@gmail.com>
2025-04-25 00:46:47 -0600

    Inicio de sesión (frontend) ya echo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,14 @@ import './App.css'
 import {BrowserRouter as Router, Routes, Route} from "react-router";
 import LandingPage from './pages/LandingPage'
 import PrimerUso from './pages/PrimerUso';
+import InicioSesion from './pages/InicioSesion'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <PrimerUso/>
+      <InicioSesion/>
     </>
   )
 }
